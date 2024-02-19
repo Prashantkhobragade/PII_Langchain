d4e2e88204fc268160fa2e2ace53f83294226d72
prashant <prashantkhobragade011@gmail.com>
2024-02-19 13:46:37 +0530

    app

--- anonymizer/anonymizer.py
+++ anonymizer/anonymizer.py
@@ -55,7 +55,7 @@ def main():
     
     st.title("PII Anonymizer")
 
-    document_content = st.text_area("Enter document content: ")
+    document_content = st.text_area("Enter document content: ", height=200, width=400)
     
     if st.button("Anonymize" ):
         if document_content:
